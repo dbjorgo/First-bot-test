7bafbcdedae6825a73321d7038510d05b5eb4452
dbjorgo <daniel.bjorgo@gmail.com>
2021-01-18 10:37:07 +0100

    First bot test

---
+++ bot.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const BOT_PREFIX = "!"
+const MOD_ME_COMMAND = 'mod-me'
+
+client.on('ready', () => {
+  console.log('Our bot is ready to go!');
+}); 
+
+client.on("message", msg => {
+  msg.channel.send("Stop deleting messages")
+})
+
+client.on('messageDelete', msg => {
+  if(msg.content == 'I love ZambieDags') {
+    msg.react('❤️');
+  }
+
+  if(msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
+    // msg.member.roles.add("800655009637072926");
+    modUser(msg.member);
+  }
+})
+
+function modUser(member) {
+  member.roles.add("800655009637072926");
+}
+
+client.login(process.env.BOT_TOKEN);
